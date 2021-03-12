7026bd216849c7b3ccb4f785661677ed68f1bbbe
weather1212 <60419960+weather1212@users.noreply.github.com>
2021-03-12 20:19:19 +0900

    Unit8
    *Unit 8. 실수 자료형 사용하기

---
+++ Unit8/judge_real_number_min_max.c
@@ -0,0 +1,21 @@
+/*	judge_real_number_min_max.c	
+#include <stdio.h>
+#include <float.h>
+
+int main(void)
+{
+    double doubleMin;
+    double doubleMax;
+    long double longDoubleMin;
+    long double longDoubleMax;
+
+    doubleMin = DBL_MIN;
+    doubleMax = DBL_MAX;
+    longDoubleMin = LDBL_MIN;
+    longDoubleMax = LDBL_MAX;
+
+    printf("%e %e\n", doubleMin, doubleMax);
+    printf("%Le %Le\n", longDoubleMin, longDoubleMax);
+
+    return 0;
+}*/
